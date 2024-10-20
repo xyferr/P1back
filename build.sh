900b1c06e3766921ee5f1709b4c10d079d8af94e
xyferr <rohit22153105@akgec.ac.in>
2024-10-20 10:22:07 +0530

    new

--- build.sh
+++ build.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
+# Ensure pip is installed
+echo "Installing pip if not present..."
+python3.9 -m ensurepip --upgrade
+
 # Build the project
 echo "Building the project..."
-python3.9 -m pip install -r requirements.txt
+python3.9 -m pip install --upgrade pip
+python3.9 -m pip install -r ../requirements.txt
 
 cd portback
 echo "Make Migrations..."
