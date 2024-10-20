c84249b90c8cb253e3981c555655495afd0fe24a
xyferr <rohit22153105@akgec.ac.in>
2024-10-20 11:55:42 +0530

    new

--- portback/portback/wsgi.py
+++ portback/portback/wsgi.py
@@ -1,23 +1,16 @@
 """
-WSGI config for portback project.
+WSGI config for backend project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
+https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
 """
 
 import os
 
-import sys
-
-# Add the portback directory to the Python path
-sys.path.append(os.path.dirname(os.path.abspath(__file__)))
-
-# from portback import app as application
-
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'portback.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
 
-application = get_wsgi_application()
+app = get_wsgi_application()
