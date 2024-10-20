2d6737beecfef16100c7c76f50cc1cd3afee25db
xyferr <rohit22153105@akgec.ac.in>
2024-10-20 11:46:04 +0530

    new

--- portback/portback/wsgi.py
+++ portback/portback/wsgi.py
@@ -9,6 +9,13 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 
 import os
 
+import sys
+
+# Add the portback directory to the Python path
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+
+# from portback import app as application
+
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'portback.settings')
