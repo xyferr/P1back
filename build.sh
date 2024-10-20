a0b4105d9028ec708a17f8ebb7e15699c89c2528
xyferr <rohit22153105@akgec.ac.in>
2024-10-20 12:31:01 +0530

    no

--- build.sh
+++ build.sh
@@ -2,19 +2,19 @@
 
 # Ensure pip is installed
 echo "Installing pip if not present..."
-python3.12 -m ensurepip --upgrade
+python3.11 -m ensurepip --upgrade
 
 # Build the project
 echo "Building the project..."
-python3.12 -m pip install --upgrade pip
-python3.12 -m pip install -r requirements.txt
+python3.11 -m pip install --upgrade pip
+python3.11 -m pip install -r requirements.txt
 
 cd portback
 echo "Make Migrations..."
-python3.12 manage.py makemigrations --noinput
-python3.12 manage.py migrate --noinput
+python3.11 manage.py makemigrations --noinput
+python3.11 manage.py migrate --noinput
 
 echo "Collect Static..."
-python3.12 manage.py collectstatic --noinput --clear
+python3.11 manage.py collectstatic --noinput --clear
 
 
