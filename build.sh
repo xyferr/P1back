4f7a5bf3ea9c3141e99d76d38f67e28d0583d472
xyferr <rohit22153105@akgec.ac.in>
2024-10-21 12:26:07 +0530

    added

--- build.sh
+++ build.sh
@@ -2,19 +2,19 @@
 
 # Ensure pip is installed
 echo "Installing pip if not present..."
-python3.10 -m ensurepip --upgrade
+python3 -m ensurepip --upgrade
 
 # Build the project
 echo "Building the project..."
-python3.10 -m pip install --upgrade pip
-python3.10 -m pip install -r requirements.txt
+python3 -m pip install --upgrade pip
+python3 -m pip install -r requirements.txt
 
 cd portback
 echo "Make Migrations..."
-python3.10 manage.py makemigrations --noinput
-python3.10 manage.py migrate --noinput
+python3 manage.py makemigrations --noinput
+python3 manage.py migrate --noinput
 
 echo "Collect Static..."
-python3.10 manage.py collectstatic --noinput --clear
+python3 manage.py collectstatic --noinput --clear
 
 
